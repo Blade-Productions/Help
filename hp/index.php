5058d197490ed95b1e4fc220f8dc587df6548c9c
Niklas Bollmer <info@niklasbollmer.de>
2011-09-30 21:28:24 +0200

    Added Basefiles

---
+++ hp/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Pic2Cloud | Kostenloser Bilder-Upload</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<script type='text/javascript' src='js/jquery.js'></script>
+<script type='text/javascript' src='js/jquery.simplemodal.js'></script>
+<script type='text/javascript' src='js/basic.js'></script>
+
+</head>
+<body>
+<div id="top_menu">
+                    <ul class="menu">
+                        <li><a href="index.html" class="nav">Startseite</a></li>
+                        <li><a href="#" class="nav">Upload</a></li>
+                        <li><a href="#" class="nav">Login/Registrierung</a></li>
+                        <li><a href="gallery.html" class="nav">Gallery</a></li>
+                    </ul>
+</div>
+<div id="main_content">
+	<div id="top_banner">
+    <a href="index.html"><img src="images/logo.png" height="130" alt="home" title="Pic2Cloud" border="0" class="logo" /></a>
+    </div>
+
+    <div id="page_content_left">
+    	<div class="title">
+        Herzlich Willkommen auf Pic2Cloud.de!
+        </div>
+        <div class="content_text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </div>
+        <div class="content_text">
+        <img src="images/illustr2.jpg" width="130" height="79"  alt="pic" title="pic" class="pic" />
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        </div>
+        <div class="title">
+        About us
+        </div>
+        <div class="content_text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </div>
+        
+        <div class="title">
+        Partners
+        </div>
+        <div class="content_text">
+        <a href="http://www.csscreme.com"><img src="images/csscreme.jpg" width="106" height="34" alt="s" title="s" class="inspiration" /></a>
+       	<a href="http://www.wallpaperstock.net"><img src="images/wallpaperstock.jpg" width="100" height="34" alt="s" title="s" class="inspiration" /></a>
+        <a href="http://www.inethouse.info"><img src="images/inethouse.jpg" width="103" height="34" alt="s" title="s" class="inspiration" /></a>
+
+        </div>
+    </div>
+    
+    <div id="page_content_right">
+    	<div class="title">
+        Letzte Uploads
+        </div>
+        <div class="content_text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        </div>
+        <div class="content_text">
+        <a href="details.html"><img src="images/pic/1.jpg" width="100" height="100"  alt="pic" title="pic" class="gallery" /></a>
+        <a href="details.html"><img src="images/pic/2.jpg" width="100" height="100"  alt="pic" title="pic" class="gallery" /></a>
+        <a href="details.html"><img src="images/pic/3.jpg" width="100" height="100"  alt="pic" title="pic" class="gallery" /></a>
+        <a href="details.html"><img src="images/pic/4.jpg" width="100" height="100"  alt="pic" title="pic" class="gallery" /></a>
+        <a href="details.html"><img src="images/pic/5.jpg" width="100" height="100"  alt="pic" title="pic" class="gallery" /></a>
+        <a href="details.html"><img src="images/pic/6.jpg" width="100" height="100"  alt="pic" title="pic" class="gallery" /></a>
+        <a href="details.html"><img src="images/pic/7.jpg" width="100" height="100"  alt="pic" title="pic" class="gallery" /></a>
+        <a href="details.html"><img src="images/pic/8.jpg" width="100" height="100"  alt="pic" title="pic" class="gallery" /></a>
+        <a href="details.html"><img src="images/pic/9.jpg" width="100" height="100"  alt="pic" title="pic" class="gallery" /></a>
+        </div>
+        <div class="more">
+        <img src="images/more.jpg" width="20" height="20" alt="more" title="more" border="0" class="more" />
+        <div class="link_more"><a href="gallery.html"> Visit gallery</a></div>
+        </div>
+    </div>
+    
+    
+    <div id="page_bottom">
+    		<div class="title">
+        	Inspiration Site
+        	</div>
+            <div class="content_text">
+			 <a href="http://www.csscreme.com"><img src="images/s5.jpg" width="125" height="40" alt="s" title="s" class="inspiration" /></a>
+             <a href="#"><img src="images/s2.jpg" width="125" height="40" alt="s" title="s" class="inspiration" /></a>
+             <a href="#"><img src="images/s3.jpg" width="125" height="40" alt="s" title="s" class="inspiration" /></a>
+             <a href="#"><img src="images/s4.jpg" width="125" height="40" alt="s" title="s" class="inspiration" /></a>
+             <a href="#"><img src="images/s1.jpg" width="125" height="40" alt="s" title="s" class="inspiration" /></a>
+       		 </div>
+<hr color="gray" height="0">
+	<center><font size="1" color="gray">&copy; 2011 Pic2Cloud.de - Pic2Cloud ist von Projekt von <a style="text-decoration:none;" target="_blank" href="http://www.2cloud-network.de/">2Cloud-Network</a>.</font></center>
+    </div>
+		
+</div>
+<div id="footer">
+<div id="footer_content">
+<div id="copyrights">
+
+</div>
+<div id="madeby">
+</div>
+</div>
+
+</div>
+</body>
+</html>
